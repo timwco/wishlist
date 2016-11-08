b0b7bb076a73158f62a798c48e113b92b23e9647
Tim Whitacre <tim@timw.co>
2016-11-08 11:24:59 -0500

    added a few things

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,14 @@
 
   const wishlist = [
     { link: 'http://l.timw.co/2eHigg0', title: 'Alton Brown: EveryDayCook'},
-    { link: 'http://l.timw.co/2eHfXda', title: 'India Pale Ale Experiment'},
     { link: 'http://l.timw.co/2eHmqot', title: 'Electric Tea Kettle'},
+    { link: 'http://l.timw.co/2fBlWOm', title: 'Lodge Cast Iron Dutch Oven (6 Quart)'},
+    { link: 'http://l.timw.co/2fBlmjl', title: 'Tramontina Non-Stick Pan (14")'},
+    { link: 'http://l.timw.co/2fBjdnM', title: '3-Blade Spiralizer'},
+    { link: 'http://l.timw.co/2eHfXda', title: 'India Pale Ale Experiment'},
     { link: 'http://l.timw.co/2eHocpz', title: 'Glencairn Crystal'},
     { link: 'http://l.timw.co/2eHjAQ1', title: 'Rosewill Mechanical Keyboard (Cherry MX Brown)'},
-    { link: 'http://l.timw.co/2fBlWOm', title: 'Lodge Cast Iron Dutch Oven (6 Quart)'},
-    { link: 'http://l.timw.co/2fBlmjl', title: 'Tramontina Non-Stick Pan (14")'}
+    { link: 'http://l.timw.co/2fBlGP1', title: 'Nintendo NES Classic'}
   ];
 
   const wlCont = document.getElementById('wishlist');
