28b668f50b74ec38ac5251d32dc5e5c642539e65
Tim Whitacre <tim@timw.co>
2018-10-25 10:44:11 -0400

    fixed links

--- src/WishList.elm
+++ src/WishList.elm
@@ -4,29 +4,29 @@ module WishList exposing (list)
 list =
     [ { title = "Books"
       , items =
-            [ { link = "http = //l.timw.co/2glqKb2", title = "Shake:  A New Perspective on Cocktails" }
-            , { link = "http = //l.timw.co/2gloMYB", title = "The 12 Bottle Bar" }
+            [ { link = "http://l.timw.co/2glqKb2", title = "Shake:  A New Perspective on Cocktails" }
+            , { link = "http://l.timw.co/2gloMYB", title = "The 12 Bottle Bar" }
             ]
       }
     , { title = "Bar"
       , items =
-            [ { link = "http = //l.timw.co/2eHocpz", title = "Glencairn Crystal" }
-            , { link = "http = //l.timw.co/2eHfXda", title = "India Pale Ale Experiment" }
+            [ { link = "http://l.timw.co/2eHocpz", title = "Glencairn Crystal" }
+            , { link = "http://l.timw.co/2eHfXda", title = "India Pale Ale Experiment" }
             ]
       }
     , { title = "Kitchen"
       , items =
-            [ { link = "http = //l.timw.co/2fBlWOm", title = "Lodge Cast Iron Dutch Oven (6 Quart)" }
+            [ { link = "http://l.timw.co/2fBlWOm", title = "Lodge Cast Iron Dutch Oven (6 Quart)" }
             ]
       }
     , { title = "Tech"
       , items =
-            [ { link = "http = //l.timw.co/2stTPZ3", title = "Xbox One Games & Stuff" }
+            [ { link = "http://l.timw.co/2stTPZ3", title = "Xbox One Games & Stuff" }
             ]
       }
     , { title = "Misc"
       , items =
-            [ { link = "http = //l.timw.co/2fwlFNO", title = "Amelie Poster" }
+            [ { link = "http://l.timw.co/2fwlFNO", title = "Amelie Poster" }
             ]
       }
     ]
