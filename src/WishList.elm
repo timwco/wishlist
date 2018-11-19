ff2bcb30829e4926ab8458f4bf83be4a93f81af0
Tim Whitacre <tim@timw.co>
2018-11-19 08:04:05 -0500

    quick update

--- src/WishList.elm
+++ src/WishList.elm
@@ -4,21 +4,15 @@ module WishList exposing (list)
 list =
     [ { title = "Books"
       , items =
-        [
-          { link = "https://l.timw.co/2qbfMf9", title = "Eat What You Watch" }
+        [ { link = "https://l.timw.co/2FuIbqK", title = "Pretty Much Everything" }
         , { link = "https://l.timw.co/2qdXj1b", title = "Essential Scratch & Sniff Guide" }
         ]
       }
     , { title = "Bar"
       , items =
-            [ { link = "http://l.timw.co/2eHocpz", title = "Glencairn Crystal" }
+            [ { link = "https://l.timw.co/2FuW6xh", title = "Glencairn Crystal" }
+            , { link = "https://l.timw.co/2Fu2k0f", title= "Colonel E.H. Taylor Small Batch" }
             , { link = "http://l.timw.co/2eHfXda", title = "India Pale Ale Experiment" }
-            ]
-      }
-    , { title = "Kitchen"
-      , items =
-            [ { link = "https://l.timw.co/2CFzZRB", title = "Tramontina 8\" Fry Pan" }
-            , { link = "https://l.timw.co/2q8tjnz", title = "Tramontina 12\" Fry Pan" }
             ]
       }
     , { title = "Tech"
@@ -32,9 +26,17 @@ list =
             , { link = "https://l.timw.co/2CEmnpD", title = "Men's Wool Runners" }
             ]
       }
-    , { title = "Collectables"
+    , {
+        title = "Games"
+      , items = 
+            [ { link = "https://l.timw.co/2FswtwZ", title = "LifeBoat (The Card Game)" }
+
+            ]
+      }
+    , { title = "Random"
       , items =
             [ { link = "http://l.timw.co/2fwlFNO", title = "Amelie Poster" }
+            , { link = "https://l.timw.co/2FuHuxE", title = "Truff Hot Sauce" }
             ]
       }
     ]
