b6c86e02fd08f7b78d2367eb290a7ab4ead6da29
Tim Whitacre <tim@timw.co>
2019-11-04 07:47:16 -0500

    updated

--- src/WishList.elm
+++ src/WishList.elm
@@ -2,21 +2,10 @@ module WishList exposing (list)
 
 
 list =
-    [ { title = "Books"
-      , items =
-        [ { link = "https://l.timw.co/2FuIbqK", title = "Pretty Much Everything" }
-        , { link = "https://www.audible.com/ep/giftcenter", title = "Audible Credits" }
-        ]
-    }
-    , { title = "Kitchen"
-      , items =
-            [ { link = "https://l.timw.co/2BIrYJZ", title = "Chemex Pour-Over"}
-            , { link = "https://www.chefsteps.com/studiopass", title = "ChefSteps Studio Pass (Online Subscription)"}
-            ]
-    }
-    , { title = "Tech"
+    [{ title = "Tech"
       , items =
             [ { link = "https://www.oculus.com/quest/", title = "Oculus Quest" }
+            , { link = "https://www.apple.com/airpods-pro/", title = "AirPods Pro" }
             , { link = "https://l.timw.co/2JmTr87", title = "Phone Photo Lens"}
             , { link = "https://www.xbox.com/en-US/games/red-dead-redemption-2", title = "Red Dead Redemption 2"}
             , { link = "https://l.timw.co/3402Bzj", title = "Echo Show 5" } 
@@ -29,6 +18,18 @@ list =
             , { link = "https://l.timw.co/2MKfho8", title = "Caper" }
             , { link = "https://l.timw.co/2PjyazW", title = "7 Wonders Duel" }
             , { link = "https://l.timw.co/2ogQQFw", title = "Great Western Trail" }
+            ]
+      }
+    , { title = "Books"
+      , items =
+            [ { link = "https://l.timw.co/2FuIbqK", title = "Pretty Much Everything" }
+            , { link = "https://www.audible.com/ep/giftcenter", title = "Audible Credits" }
+            ]
+      }
+    , { title = "Kitchen"
+      , items =
+            [ { link = "https://l.timw.co/2BIrYJZ", title = "Chemex Pour-Over"}
+            , { link = "https://www.chefsteps.com/studiopass", title = "ChefSteps Studio Pass (Online Subscription)"}
             ]
       }
     ]
